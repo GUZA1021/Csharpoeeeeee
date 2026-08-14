9b689476925ae47c3668bdcdc0d97ee27304eb33
GUZA1021 <karimamin910@outlook.dk>
2026-08-14 19:30:50 +0200

    update

--- jobapplication/jobapplication/Program.cs
+++ jobapplication/jobapplication/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace jobapplication
+﻿using System.Linq;
+using System.Linq.Expressions;
+namespace jobapplication
 {
     public class JobApplication
     {
-        public int Id { get; set; }
+        public int Id { get; private set;} // Private set gør at det er kun selve klassen og ingen andre som kan redigere variablen
         public string Company { get; set; }
         public string Position { get; set; }
         public Status Status { get; set; }
@@ -12,6 +14,10 @@
             Company = company;
             Position = position;
             Status = Status.Applied;
+            if (string.IsNullOrWhiteSpace(company))
+            {
+                throw new ArgumentException("Wela eri det forkert");
+            }
         }
         public string Jobs()
         {
@@ -23,8 +29,19 @@
     {
         static void Main(string[] args)
         {
-            JobApplication Google = new JobApplication("Google","Backend");
-            Console.WriteLine(Google.Jobs());
+            List<JobApplication> list = new List<JobApplication> {new JobApplication("Google","Backend"), new JobApplication(null, "Frontend"), new JobApplication("youtube", "backend") };
+            list[1].Status = Status.Interview;
+            try
+            {
+                int i = int.Parse(Console.ReadLine());
+            }
+            catch (Exception a)
+            {
+                throw new Exception("hej");
+            }
+            foreach (JobApplication app in list) {
+                Console.WriteLine(app.Jobs());
+            }
         }
     }
 
