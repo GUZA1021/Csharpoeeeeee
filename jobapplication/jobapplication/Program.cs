a04642b089f3a933c26ef39c4b4d6f65aa80dfbe
GUZA1021 <karimamin910@outlook.dk>
2026-08-14 18:38:18 +0200

    job

---
+++ jobapplication/jobapplication/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace jobapplication
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public Status Status { get; set; }
+
+        public JobApplication(string company, string position)
+        {
+            Company = company;
+            Position = position;
+            Status = Status.Applied;
+        }
+        public string Jobs()
+        {
+            return $"Ansøgning hos {Company} as a {Position} and the status is : {Status}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JobApplication Google = new JobApplication("Google","Backend");
+            Console.WriteLine(Google.Jobs());
+        }
+    }
+
+}
+
---
+++ jobapplication/jobapplication/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jobapplication
+{
+    public enum Status
+    {
+        Applied,
+        Rejected,
+        Interview,
+        Accepted,
+    }
+}
