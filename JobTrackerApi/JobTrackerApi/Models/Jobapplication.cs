28fbf6ff2cd3f15eb54303b15ead25151ca25302
GUZA1021 <karimamin910@outlook.dk>
2026-08-15 23:30:10 +0200

    Made data annotations for jobapplication fields and fixed some bugs

--- JobTrackerApi/JobTrackerApi/Models/Jobapplication.cs
+++ JobTrackerApi/JobTrackerApi/Models/Jobapplication.cs
@@ -1,21 +1,20 @@
-﻿using System.Linq;
-using System.Linq.Expressions;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace JobTrackerApi.Models
 {
     public class JobApplication
     {
         public int Id { get; private set; } // Private set gør at det er kun selve klassen og ingen andre som kan redigere variablen
+        [Required]
         public string Company { get; set; }
+        [Required]
         public string Position { get; set; }
+        [EnumDataType(typeof(Status))]
         public Status Status { get; set; }
 
         public JobApplication(string company, string position)
         {
-            if (string.IsNullOrWhiteSpace(company) || string.IsNullOrWhiteSpace(position))
-            {
-                throw new ArgumentException("Wela eri det forkert");
-            }
+
             Company = company;
             Position = position;
             Status = Status.Applied;
